4fe10eb9d0e2fb88b500100a017bcc55aa58fb59
Moises Zaragoza <mZaragoza@circletechfl.com>
2015-03-29 03:33:42 -0400

    TWILIO

---
+++ app/models/sms.rb
@@ -0,0 +1,19 @@
+class Sms
+
+  def self.send_message(recipient, message)
+    #if Rails.env.test? || Rails.env.development?
+      unless ENV['TWILIO_TEST_NUMBERS'].include?(recipient)
+        recipient = ENV['TWILIO_TEST_NUMBER']
+      end
+    #end
+
+    twilio_client = Twilio::REST::Client.new(ENV['TWILIO_ACCOUNT_SID'], ENV['TWILIO_AUTH_TOKEN'])
+
+    twilio_client.account.messages.create(
+      from: ENV['TWILIO_FROM_PHONE'],
+      to: recipient,
+      body: message
+    )
+  end
+end
+
