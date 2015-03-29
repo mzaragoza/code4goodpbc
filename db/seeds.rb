7f439da6419da66cfc8824629c309e9f709e8bcf
Aldo Delgado <aldo@osstek.com>
2015-03-29 12:41:45 -0400

    WIP

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,3 +14,35 @@ o = Organization.create(
   zip: '33015'
 )
 Receiver.create(email: 'admin@test.com', password: 'password', organization_id: o.id)
+o = Organization.create(
+  name: 'Dominos Pizza',
+  address: '1336 S Military Trail',
+  city: 'West Palm Beach',
+  state: 'Florida',
+  zip: '33415'
+)
+Provider.create(email: 'admin@test1.com', password: 'password', organization_id: o.id)
+o = Organization.create(
+  name: 'Subway',
+  address: '6294 Forest Hill Blvd',
+  city: 'Greenacres',
+  state: 'Florida',
+  zip: '33415'
+)
+Provider.create(email: 'admin@test2.com', password: 'password', organization_id: o.id)
+o = Organization.create(
+  name: 'Garcias Seafood Grille & Fish Market',
+  address: '398 NW North River Dr',
+  city: 'Miami',
+  state: 'Florida',
+  zip: '33128'
+)
+Provider.create(email: 'admin@test3.com', password: 'password', organization_id: o.id)
+o = Organization.create(
+  name: 'Publix Super Market at Miami Shores',
+  address: '9050 Biscayne Blvd',
+  city: 'Miami Shores',
+  state: 'Florida',
+  zip: '33138'
+)
+Provider.create(email: 'admin@test4.com', password: 'password', organization_id: o.id)
